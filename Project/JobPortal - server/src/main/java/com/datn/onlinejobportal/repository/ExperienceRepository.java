2a39bcbb328884c4f4877751d99ef2579ae769b1
TranPhuongNam2112 <34429397+TranPhuongNam2112@users.noreply.github.com>
2020-05-11 17:15:10 +0700

    Fix

--- Project/JobPortal - server/src/main/java/com/datn/onlinejobportal/repository/ExperienceRepository.java
+++ Project/JobPortal - server/src/main/java/com/datn/onlinejobportal/repository/ExperienceRepository.java
@@ -11,11 +11,10 @@ import com.datn.onlinejobportal.payload.ExperienceResponse;
 
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
 	
-	@Query("Select e From Experience e LEFT JOIN e.candidate c LEFT JOIN c.user u Where u.id = ?1")
+	@Query("Select new com.datn.onlinejobportal.payload.ExperienceResponse(e.id, e.company_name, e.job_title, e.start_date, e.end_date, e.description) From Experience e LEFT JOIN e.candidate c LEFT JOIN c.user u Where u.id = ?1")
 	List<ExperienceResponse> getExperienceByUser(Long userId);
 	
-	@Query("Select new com.datn.onlinejobportal.payload.ExperienceResponse(e.id, e.company_name, e.job_title, e.start_date, e.end_date, e.description) "
-			+ "From Experience e LEFT JOIN e.candidate c LEFT JOIN c.user u Where u.id = :userId And e.id = :experienceId")
+	@Query("Select e From Experience e LEFT JOIN e.candidate c LEFT JOIN c.user u Where u.id = :userId And e.id = :experienceId")
 	Experience getExperienceByUserIdAndExperienceId(@Param("userId") Long userId, @Param("experienceId") Long experienceId);
 
 }
